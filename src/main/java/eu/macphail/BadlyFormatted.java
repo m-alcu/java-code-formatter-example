87766e5d7d692aac667f95e4d3998484a4dbc1db
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2019-10-01 23:18:38 +0200

    First Commit

---
+++ src/main/java/eu/macphail/BadlyFormatted.java
@@ -0,0 +1,12 @@
+package eu.macphail;
+
+public class BadlyFormatted {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+
+    private String getString() {
+        return "heleloworld";
+    }
+}
