b845006f205ff01e8d510092eb55ee864d490e65
m-alcu <martin.alcubierre@gmail.com>
2021-05-06 17:06:21 +0200

    pa dentro

--- src/main/java/eu/macphail/BadlyFormatted.java
+++ src/main/java/eu/macphail/BadlyFormatted.java
@@ -3,7 +3,7 @@ package eu.macphail;
 public class BadlyFormatted {
 
 	public static void main(String[] args) {
-		System.out.println("hi hi");
+		System.out.println("hi hi hi");
 	}
 
 	private String getString() {
